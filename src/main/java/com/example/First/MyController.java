1934320070f6de9f4fff2d4cf65b040f597a2898
Shashank Singhal <shashankjtp@gmail.com>
2021-12-11 22:57:18 +0530

    second commit

--- src/main/java/com/example/First/MyController.java
+++ src/main/java/com/example/First/MyController.java
@@ -1,6 +1,8 @@
 package com.example.First;
 
+import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +13,11 @@ public class MyController {
 		return "Hello I am SHashank";
 	}
 	
-	
+   @PostMapping("/hello1")
+	public User createValue() {
+		User u=new User();
+		u.setName("shashank");
+		u.setPassword("dssd");
+		return u;
+	}
 }
