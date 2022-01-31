126306e86e87cc1277db929bb50d0d02518fe22d
Shashank Singhal <shashankjtp@gmail.com>
2022-01-31 18:32:03 +0530

    change password

--- src/main/java/com/example/First/MyController.java
+++ src/main/java/com/example/First/MyController.java
@@ -17,7 +17,7 @@ public class MyController {
 	public User createValue() {
 		User u=new User();
 		u.setName("shashank");
-		u.setPassword("dssd");
+		u.setPassword("12345678");
 		return u;
 	}
 }
